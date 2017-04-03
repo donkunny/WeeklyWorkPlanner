89ed0d115d2f15e49a3d8cad072595aace183541
ROOM3_17 <ROOM3_17@ROOM3_17-PC>
2017-04-03 18:32:10 +0900

    empcontroller

--- WeeklyWorkPlanner/src/main/java/com/lotte/controller/EmpController.java
+++ WeeklyWorkPlanner/src/main/java/com/lotte/controller/EmpController.java
@@ -1,28 +1,20 @@
-/**
- * @author 김동혁
- */
 package com.lotte.controller;
 
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.lotte.testService.TestService;
 
 @Controller
-public class TestController {
+public class EmpController {
 
 	@Inject
 	TestService service;
 	
-	@RequestMapping(value="/listAll")
-	public void listAll(Model model) throws Exception{
-		model.addAttribute("list", service.listAll());
-	}
-	
 	@RequestMapping(value="/index")
-	public void index(Model model) throws Exception{
+	public void test(){
+		
 	}
 }
